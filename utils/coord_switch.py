54322f696ba686583c461ea4a1b9c0fa4bddf4bb
Ovlic <74332950+Ovlic@users.noreply.github.com>
2025-02-25 15:09:45 -0800

    rename function

--- utils/coord_switch.py
+++ utils/coord_switch.py
@@ -1,26 +1,51 @@
 
+import os
 # Input: geojson
 # Output: same geojson with lat and lon flipped
 
-def coord_switch(geojson):
+def switch_coords(geojson):
     for feature in geojson["features"]:
         for i in range(len(feature["geometry"]["coordinates"])):
+            # if len(feature["geometry"]["coordinates"][i]) == 1:
+            #     for j in range(len(feature["geometry"]["coordinates"][i])):
+            #         feature["geometry"]["coordinates"][i][j] = feature["geometry"]["coordinates"][i][j][
+            #             ::-1
+            #         ]
             feature["geometry"]["coordinates"][i] = feature["geometry"]["coordinates"][i][
                 ::-1
             ]
     return geojson
 
-# Input: shapes/CE.geojson
-
+# Example usage:
+# Input: CE.geojson
 # Output: updated_CE.geojson with lat and lon flipped
-if name == "__main__":
+if __name__ == "__main__":
     import json
-    with open("shapes/CE.geojson") as f:
+    # Loop through files in data/qgis_track_data and flip the coordinates
+    """
+    for filename in os.listdir("data/qgis_track_data/bart"):
+        if not filename.endswith(".geojson"):
+            continue
+        print(f"Processing {filename}")
+        with open(f"data/qgis_track_data/bart/{filename}") as f:
+            geojson = json.load(f)
+
+        updated_geojson = switch_coords(geojson)
+
+        
+        with open(f"data/track_data/bart/{filename}", "w") as f:
+            json.dump(updated_geojson, f)
+        
+        print("Done")
+        """
+
+    with open("data/lines/ace.geojson") as f:
         geojson = json.load(f)
 
-    updated_geojson = coord_switch(geojson)
+    updated_geojson = switch_coords(geojson)
 
-    with open("updated_CE.geojson", "w") as f:
+    with open("data/track_data/ace.geojson", "w") as f:
         json.dump(updated_geojson, f)
-
+    
+    # """
     
